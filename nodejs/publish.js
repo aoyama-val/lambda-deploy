f2f18e3528155a6386a9e53f484ddfaf067d4b60
Aoyama, Shotaro <aoyama@val.co.jp>
2019-01-30 10:10:36 +0900

    improve publish

--- nodejs/publish.js
+++ nodejs/publish.js
@@ -1,50 +1,53 @@
-var proxy = process.env.https_proxy ? process.env.https_proxy : '';
+var aws = require('aws-sdk');
+var fs = require('fs');
+var path = require('path');
+var lambdaConfig = require(process.cwd() + '/lambda-config.json');
 
-var aws = require('aws-sdk'),
-  fs = require('fs'),
-  lambdaConfig = require(process.cwd() + '/lambda-config'),
-  path = require('path');
-
-var deployEnv = lambdaConfig.environments[process.env.DEPLOY];
-if (!deployEnv) {
-  console.log('環境変数 $DEPLOY が正しく設定されていません [', process.env.DEPLOY, ']');
-  console.log('次のいずれかの値を設定してください: ' + Object.keys(lambdaConfig.environments).join(', '));
-  process.exit(1);
-}
-
-var lambda = new aws.Lambda({
-  region: lambdaConfig.region,
-  httpOptions: {proxy: proxy}
-});
-var zipPath = 'pkg/' + path.basename(process.cwd()) + '.zip';
-
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#updateFunctionCode-property
-var params = {
-  FunctionName: deployEnv.functionName,
-  ZipFile: fs.readFileSync(zipPath)
-};
-lambda.updateFunctionCode(params, function (err, data) {
-  if (err) {
-    console.log(err, err.stack);
-  } else {
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#updateFunctionConfiguration-property
-    var configuration = {
-      Description: getConfig['description'],
-      FunctionName: getConfig['functionName'],
-      Handler: getConfig['handlerFile'] + '.' + getConfig['handlerMethod'],
-      MemorySize: getConfig['memorySize'],
-      Role: getConfig['role'],
-      Runtime: getConfig['runtime'],
-      Timeout: getConfig['timeout'],
-      Environment: { Variables: { LAMBDA_ENV: process.env.DEPLOY } },
-    };
-    lambda.updateFunctionConfiguration(configuration, function (err, data) {
-      if (err) console.log(err, err.stack);
-      else console.log(data);
-    });
+function publish() {
+  var deployEnv = lambdaConfig.environments[process.env.DEPLOY];
+  if (!deployEnv) {
+    console.log('環境変数 $DEPLOY が正しく設定されていません [', process.env.DEPLOY, ']');
+    console.log('次のいずれかの値を設定してください: ' + Object.keys(lambdaConfig.environments).join(', '));
+    process.exit(1);
   }
-});
+
+  var proxy = process.env.https_proxy ? process.env.https_proxy : '';
+  var lambda = new aws.Lambda({
+    region: lambdaConfig.region,
+    httpOptions: {proxy: proxy}
+  });
+  var zipPath = 'pkg/' + path.basename(process.cwd()) + '.zip';
+
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#updateFunctionCode-property
+  var params = {
+    FunctionName: deployEnv.functionName,
+    ZipFile: fs.readFileSync(zipPath)
+  };
+  lambda.updateFunctionCode(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#updateFunctionConfiguration-property
+      var configuration = {
+        Description: getConfig['description'],
+        FunctionName: getConfig['functionName'],
+        Handler: getConfig['handlerFile'] + '.' + getConfig['handlerMethod'],
+        MemorySize: getConfig['memorySize'],
+        Role: getConfig['role'],
+        Runtime: getConfig['runtime'],
+        Timeout: getConfig['timeout'],
+        Environment: { Variables: { LAMBDA_ENV: process.env.DEPLOY } },
+      };
+      lambda.updateFunctionConfiguration(configuration, function (err, data) {
+        if (err) console.log(err, err.stack);
+        else console.log(data);
+      });
+    }
+  });
+}
 
 function getConfig(key) {
   return (typeof(deployEnv[key]) != 'undefined') ? deployEnv[key] : lambdaConfig[key];
 }
+
+publish();
